d3c26b30cd5693a05dbf2143d4187b2c25e67023
Patrik Mirzai <48419053+mirzaipatrik@users.noreply.github.com>
2024-05-07 21:19:37 +0300

    feat: add log predictive score

--- Functions/main.jl
+++ Functions/main.jl
@@ -30,19 +30,31 @@ include("backward_function.jl")
 include("main_function.jl")
 
 dat = CSV.read("Functions/My_data.csv", DataFrame, delim=';', decimal=',', ignoreemptylines=true)
-plot(dat[:, 4], line = (:line), label = "Volume")
+# plot(dat[:, 4], line = (:line), label = "Volume")
 
 
 n_states = 3  #Number of states
-n_iter = 2000  #Number of iterations
+n_iter = 200  #Number of iterations
 Γ = [0.1 0.7 0.2; 0.3 0.4 0.3; 0.2 0.2 0.6];  #Transition prob. matrix
 #These are our hyperparameters:
 υ_hyper = [1,1,1]
 σ2_hyper = [1,1,1]
 μ_hyper = [4, 5, 6]
 κ_hyper = [1,1,1]
+lps_iterations = 20
 
 
-burn_in = 1000
+burn_in = 200
 
-MCMC_sim = main_function(n_states, n_iter, burn_in, Γ, dat[:, 4], υ_hyper, σ2_hyper, μ_hyper, κ_hyper)
+MCMC_sim = main_function(
+    n_states,
+    n_iter,
+    burn_in,
+    Γ,
+    dat[:,4],
+    υ_hyper,
+    σ2_hyper,
+    μ_hyper,
+    κ_hyper,
+    lps_iterations
+)
